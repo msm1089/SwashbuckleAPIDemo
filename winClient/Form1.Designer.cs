3faa75651a8a47ec32ccd63790daaa0a86347ee9
Mark McPherson <msm1089@yahoo.co.uk>
2018-07-27 17:11:27 +0100

    initial commit

---
+++ winClient/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace winClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdAllValues = new System.Windows.Forms.Button();
+            this.cmdValue = new System.Windows.Forms.Button();
+            this.cmdUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmdAllValues
+            // 
+            this.cmdAllValues.Location = new System.Drawing.Point(24, 22);
+            this.cmdAllValues.Name = "cmdAllValues";
+            this.cmdAllValues.Size = new System.Drawing.Size(75, 23);
+            this.cmdAllValues.TabIndex = 0;
+            this.cmdAllValues.Text = "All Values";
+            this.cmdAllValues.UseVisualStyleBackColor = true;
+            this.cmdAllValues.Click += new System.EventHandler(this.cmdAllValues_Click);
+            // 
+            // cmdValue
+            // 
+            this.cmdValue.Location = new System.Drawing.Point(105, 22);
+            this.cmdValue.Name = "cmdValue";
+            this.cmdValue.Size = new System.Drawing.Size(75, 23);
+            this.cmdValue.TabIndex = 1;
+            this.cmdValue.Text = "Single Value";
+            this.cmdValue.UseVisualStyleBackColor = true;
+            this.cmdValue.Click += new System.EventHandler(this.cmdValue_Click);
+            // 
+            // cmdUpdate
+            // 
+            this.cmdUpdate.Location = new System.Drawing.Point(186, 22);
+            this.cmdUpdate.Name = "cmdUpdate";
+            this.cmdUpdate.Size = new System.Drawing.Size(75, 23);
+            this.cmdUpdate.TabIndex = 2;
+            this.cmdUpdate.Text = "Update";
+            this.cmdUpdate.UseVisualStyleBackColor = true;
+            this.cmdUpdate.Click += new System.EventHandler(this.cmdUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmdUpdate);
+            this.Controls.Add(this.cmdValue);
+            this.Controls.Add(this.cmdAllValues);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdAllValues;
+        private System.Windows.Forms.Button cmdValue;
+        private System.Windows.Forms.Button cmdUpdate;
+    }
+}
+
---
+++ winClient/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using libTools;
+using svcRest;
+
+namespace winClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void cmdAllValues_Click(object sender, EventArgs e)
+        {
+            SvcRestClient client =
+                new SvcRestClient(new Uri("http://localhost/webDemo"),
+                    new AnonymousCredential());
+            var result = client.Values.Get();
+
+            foreach (var x in result)
+            {
+                MessageBox.Show(x);
+            }
+        }
+
+        private async void cmdValue_Click(object sender, EventArgs e)
+        {
+            SvcRestClient client =
+                new SvcRestClient(new Uri("http://localhost/webDemo"),
+                  new AnonymousCredential());
+            var result = client.Values.GetByid(1);
+            MessageBox.Show(result);
+        }
+
+        private async void cmdUpdate_Click(object sender, EventArgs e)
+        {
+            SvcRestClient client =
+                new SvcRestClient(new Uri("http://localhost/webDemo"),
+                  new AnonymousCredential());
+            client.Values.PutByidvalue(1, "New Value");
+
+            MessageBox.Show("Change Completed!");
+        }
+    }
+}
